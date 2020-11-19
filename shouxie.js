b9f8834ca01e83c268c5d0b7885195f84160bde5
wuhao <849315604@qq.com>
2020-11-19 21:42:05 +0800

    11.19-2

--- shouxie.js
+++ shouxie.js
@@ -31,13 +31,33 @@ A = Animal.bind(Cat,name)
 a = new A(age)
 function mybind(context,...args1){
   if(typeof this !== "function") throw new Error('not function')
-  var self = this  //接收外部是谁调用bind
-  var Fn = function(...args2){
-    return self.apply(this instanceof context? this : context,[...args1].contact([...args2]))//判断是new绑定还是直接绑定，new绑定就绑定new调用的this，直接绑定的话 this指向上下文
+  let self = this  //接收外部是谁调用bind
+  let Fn = function(...args2){
+    return self.apply(this instanceof Fn? this : context,[...args1].contact([...args2]))//判断是new绑定还是直接绑定，new绑定就绑定new调用的this，直接绑定的话 this指向上下文
   }
-  var Fmiddle = function(){}
+  let Fmiddle = function(){}    //如果调用bind的方法是原型上的方法，需要将返回函数的原型链指向调用bind的方法 使得A的实例可以使用Animal的属性
   Fmiddle = this.prototype
   Fn.prototype = new Fmiddle();
   return Fn;
 }
   
+function mycall(obj,...args){
+  let obj = obj || window
+  obj.p = this    //调用函数添加进属性 并且调用call的函数里面的this指向obj
+  
+  let res = obj.p(...args);        //执行调用函数
+  delete obj.p    //执行完毕后删除该属性
+  return res
+}
+
+function myapply(obj,arr){
+  let obj = obj || window
+  obj.p = this    //调用函数添加进属性 并且调用call的函数里面的this指向obj
+  let res
+  if(!arr){
+    res = obj.p()
+  }
+  else {res = obj.p(...arr);}        //执行调用函数
+  delete obj.p    //执行完毕后删除该属性
+  return res
+}
