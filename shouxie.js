02efef51ab04bf32cc7413a505a74faa0177d1b6
wuhao <849315604@qq.com>
2020-11-19 20:25:40 +0800

    11.19

--- shouxie.js
+++ shouxie.js
@@ -23,8 +23,21 @@ function deepcopy(obj){
   for(const [k,v] of Object.entries(obj)){ //无论数组还是对象都可以被解构
     res[k] = typeof v == 'object'?deepcopy(v):v;
   }
-
   return res;
 }
 
+
+A = Animal.bind(Cat,name)
+a = new A(age)
+function mybind(context,...args1){
+  if(typeof this !== "function") throw new Error('not function')
+  var self = this  //接收外部是谁调用bind
+  var Fn = function(...args2){
+    return self.apply(this instanceof context? this : context,[...args1].contact([...args2]))//判断是new绑定还是直接绑定，new绑定就绑定new调用的this，直接绑定的话 this指向上下文
+  }
+  var Fmiddle = function(){}
+  Fmiddle = this.prototype
+  Fn.prototype = new Fmiddle();
+  return Fn;
+}
   
